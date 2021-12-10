63c0225ec8ae2e788878d90c639776289a3ac92a
Sylvie-B <unsolo@hotmail.fr>
2021-12-10 09:38:20 +0100

    place & close window menubar -> no

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2')
 
-let features = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+let features = "menubar=no,location=yes,resizable=yes,scrollbars=yes,status=yes";
 
 btn1.addEventListener("click", function (){
     let test = window.open('https://www.tinkercad.com/', "", features);
-    test.resizeTo(200, 100);
-    test.moveTo(600,600);
+    test.resizeTo(500, 600);
+    test.moveTo(100,100);
 
     btn2.addEventListener('click', function (){
         test.close();
