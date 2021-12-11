cd11dd1eae815aa44694aef2fabdd1e90bbb48d6
Sylvie-B <unsolo@hotmail.fr>
2021-12-11 14:11:00 +0100

    move by

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let btn1 = document.querySelector('#btn1');
 let btn2 = document.querySelector('#btn2')
 
 btn1.addEventListener("click", function (){
-    let features = "menubar=no,location=yes,resizable=yes,scrollbars=yes,status=yes";
+    let features = "menubar=no,location=yes,resizable=yes,with=500px,height=500px,scrollbars=yes,status=yes";
     let test = window.open('https://www.tinkercad.com/', "", features);
     test.resizeTo(500, 600);
-    test.moveTo(300,100);
+    test.moveBy(500,100);
 
     btn2.addEventListener('click', function (){
         test.close();
